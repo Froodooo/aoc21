31713375806c1e38a54e18b967aa4afecf02c3d9
Froodooo <yordiverkroost@hotmail.com>
2021-12-04 09:40:07 +0100

    Some day 4 cleanup

---
+++ day4/common.ts
@@ -0,0 +1,38 @@
+import { Bingo } from "./bingo.ts";
+import { Board } from "./board.ts";
+import { BoardNumber } from "./board_number.ts";
+import { chunked } from "https://deno.land/std@0.106.0/collections/chunked.ts";
+
+export function readDraw(input: string): number[] {
+  return input.split("\n")[0].split(",").map((number) => parseInt(number));
+}
+
+export function readBoards(input: string): Board[] {
+  const lines = input.split("\n").filter((line) => line != "");
+  const rawBoards = chunked(lines.slice(1), 5)
+    .map((rawBoard) => {
+      let boardNumbers = [];
+      for (let i = 0; i < 5; i++) {
+        let boardLine = rawBoard[i].split(" ").filter((number) => number != "");
+        for (let j = 0; j < 5; j++) {
+          boardNumbers.push(
+            new BoardNumber(parseInt(boardLine[j].trim()), j, i),
+          );
+        }
+      }
+      return boardNumbers;
+    });
+
+  return rawBoards.map((boardNumbers) => new Board(boardNumbers));
+}
+
+export function calculateResult(bingo: Bingo): number | undefined {
+  if (bingo.lastWinner) {
+    const sumUnmarked = bingo.lastWinner.numbers.filter((number) =>
+      !number.marked
+    )
+      .map((number) => number.value).reduce((sum, current) => sum + current, 0);
+    const lastDraw = bingo.lastDraw;
+    return sumUnmarked * lastDraw;
+  }
+}
--- day4/day4a.ts
+++ day4/day4a.ts
@@ -1,30 +1,5 @@
-import { Board } from "./board.ts";
-import { BoardNumber } from "./board_number.ts";
 import { Bingo } from "./bingo.ts";
-import { chunked } from "https://deno.land/std@0.106.0/collections/chunked.ts";
-
-export function readDraw(input: string): number[] {
-  return input.split("\n")[0].split(",").map((number) => parseInt(number));
-}
-
-export function readBoards(input: string): Board[] {
-  const lines = input.split("\n").filter((line) => line != "");
-  const rawBoards = chunked(lines.slice(1), 5)
-    .map((rawBoard) => {
-      let boardNumbers = [];
-      for (let i = 0; i < 5; i++) {
-        let boardLine = rawBoard[i].split(" ").filter((number) => number != "");
-        for (let j = 0; j < 5; j++) {
-          boardNumbers.push(
-            new BoardNumber(parseInt(boardLine[j].trim()), j, i),
-          );
-        }
-      }
-      return boardNumbers;
-    });
-
-  return rawBoards.map((boardNumbers) => new Board(boardNumbers));
-}
+import { calculateResult, readBoards, readDraw } from "./common.ts";
 
 const input = await Deno.readTextFile("./day4/day4.txt");
 const draw = readDraw(input);
@@ -32,12 +7,4 @@ const boards = readBoards(input);
 const bingo = new Bingo(boards, draw);
 
 bingo.play();
-
-if (bingo.lastWinner) {
-  const sumUnmarked = bingo.lastWinner.numbers.filter((number) =>
-    !number.marked
-  )
-    .map((number) => number.value).reduce((sum, current) => sum + current, 0);
-  const lastDraw = bingo.lastDraw;
-  console.log(sumUnmarked * lastDraw);
-}
+console.log(calculateResult(bingo));
