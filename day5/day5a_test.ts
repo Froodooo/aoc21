9682dfa0d587dbc218820d56d90c0a686c264405
Froodooo <yordiverkroost@hotmail.com>
2021-12-05 08:18:59 +0100

    Refactor day 5

--- day5/day5a_test.ts
+++ day5/day5a_test.ts
@@ -1,6 +1,22 @@
-// import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
+import { visitPoints } from "./common.ts";
 
-// Deno.test("Example 1", async () => {
-//   const input = await Deno.readTextFile("./day5/day5_ex.txt");
-//   assertEquals(true, true);
-// });
+Deno.test("Example 1", async () => {
+  const input = await Deno.readTextFile("./day5/day5_ex.txt");
+  const paths = input.split("\n");
+  const visitedPoints = visitPoints(paths, true);
+
+  const multipleOverlaps =
+    [...visitedPoints.values()].filter((point) => point > 1).length;
+  assertEquals(multipleOverlaps, 5);
+});
+
+Deno.test("Day 5", async () => {
+  const input = await Deno.readTextFile("./day5/day5.txt");
+  const paths = input.split("\n");
+  const visitedPoints = visitPoints(paths, true);
+
+  const multipleOverlaps =
+    [...visitedPoints.values()].filter((point) => point > 1).length;
+  assertEquals(multipleOverlaps, 4993);
+});
