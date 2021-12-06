b11c56fd76d6753ab9c7b606bdd6e83b758b9e42
Froodooo <yordiverkroost@hotmail.com>
2021-12-06 07:59:08 +0100

    Split logic into functions

--- day6/common.ts
+++ day6/common.ts
@@ -2,24 +2,43 @@ const INTERVAL = 6;
 const MAX_INTERVAL = 8;
 
 export function solve(input: string, days: number): number {
-  const fish = input.split(",").map((f) => parseInt(f)).reduce(
+  const fish = readFish(input);
+
+  for (let _i = 0; _i < days; _i++) {
+    const zeroTimerFish = getZeroTimerFish(fish);
+
+    updateTimers(fish);
+    resetFishTimers(fish, zeroTimerFish);
+    addFish(fish, zeroTimerFish);
+  }
+
+  return fish.reduce((a, b) => a + b);
+}
+
+function getZeroTimerFish(fish: number[]): number {
+  return fish[0];
+}
+
+function updateTimers(fish: number[]): void {
+  for (let j = 0; j < fish.length - 1; j++) {
+    fish[j] = fish[j + 1];
+  }
+}
+
+function resetFishTimers(fish: number[], zeroTimerFish: number): void {
+  fish[INTERVAL] += zeroTimerFish;
+}
+
+function addFish(fish: number[], zeroTimerFish: number): void {
+  fish[fish.length - 1] = zeroTimerFish;
+}
+
+function readFish(input: string): number[] {
+  return input.split(",").map((f) => parseInt(f)).reduce(
     (fishTimers, f) => {
       fishTimers[f] += 1;
       return fishTimers;
     },
     new Array(MAX_INTERVAL + 1).fill(0),
   );
-
-  for (let i = 0; i < days; i++) {
-    const zeroTimerFish = fish[0];
-
-    for (let j = 0; j < fish.length - 1; j++) {
-      fish[j] = fish[j + 1];
-    }
-
-    fish[INTERVAL] += zeroTimerFish;
-    fish[fish.length - 1] = zeroTimerFish;
-  }
-
-  return fish.reduce((a, b) => a + b);
 }
