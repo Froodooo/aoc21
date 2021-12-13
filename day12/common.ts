0b8becc274ae17d902999f2b99d71b18a85638a8
Froodooo <yordiverkroost@hotmail.com>
2021-12-13 10:09:56 +0100

    Format day 12

--- day12/common.ts
+++ day12/common.ts
@@ -25,8 +25,12 @@ export function readGraph(input: string): Map<string, Set<string>> {
   input.split("\n").forEach((vertex) => {
     const [start, end] = vertex.split("-");
 
-    if (end !== "start") graph.set(start, (graph.get(start) ?? new Set<string>()).add(end))
-    if (start !== "start") graph.set(end, (graph.get(end) ?? new Set<string>()).add(start));
+    if (end !== "start") {
+      graph.set(start, (graph.get(start) ?? new Set<string>()).add(end));
+    }
+    if (start !== "start") {
+      graph.set(end, (graph.get(end) ?? new Set<string>()).add(start));
+    }
   });
 
   graph.delete("end");
