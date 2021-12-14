8416a3fc6865fd9eb885ae887574ca387c02af3d
Froodooo <yordiverkroost@hotmail.com>
2021-12-14 09:03:12 +0100

    Solutions for day 14

---
+++ day14/common.ts
@@ -0,0 +1,83 @@
+import { frequencies } from "https://deno.land/x/pine@0.5.0/frequencies.ts";
+import { slidingWindows } from "https://deno.land/std@0.108.0/collections/mod.ts";
+
+type rules = Map<string, string>;
+type pairs = Map<string, number>;
+
+function readRules(input: string): rules {
+  return input.split("\n").reduce((rules, line) => {
+    const [pair, insertion] = line.split(" -> ");
+    rules.set(pair, insertion);
+    return rules;
+  }, new Map<string, string>());
+}
+
+function stepPairCount(
+  pairCount: pairs,
+  pair: string,
+  count: number,
+  letter: string,
+) {
+  const splitPair = pair.split("");
+  const newPairs = [splitPair[0] + letter, letter + splitPair[1]];
+  newPairs.forEach((newPair) => {
+    const currentSize = pairCount.get(newPair) ?? 0;
+    pairCount.set(newPair, currentSize + count);
+  });
+
+  return pairCount;
+}
+
+function stepLetterCount(
+  letterCount: pairs,
+  letter: string,
+  count: number,
+): pairs {
+  const currentLetterSize = letterCount.get(letter) ?? 0;
+  letterCount.set(letter, currentLetterSize + count);
+  return letterCount;
+}
+
+function step(
+  pairCount: pairs,
+  letterCount: pairs,
+  rules: rules,
+): [pairs, pairs] {
+  let newPairCount = new Map<string, number>();
+  for (let [pair, count] of Array.from(pairCount.entries())) {
+    const letter = rules.get(pair) ?? "";
+
+    letterCount = stepLetterCount(letterCount, letter, count);
+    newPairCount = stepPairCount(newPairCount, pair, count, letter);
+  }
+  return [newPairCount, letterCount];
+}
+
+function gePairCount(polymer: string): pairs {
+  const pairs = slidingWindows(polymer.split(""), 2);
+  return pairs.reduce((pairCount, pair) => {
+    const joinedPair = pair.join("");
+    const currentSize = pairCount.get(joinedPair) ?? 0;
+    pairCount.set(joinedPair, currentSize + 1);
+    return pairCount;
+  }, new Map<string, number>());
+}
+
+function getLetterCount(polymer: string): pairs {
+  return frequencies(polymer.split(""));
+}
+
+export function run(input: string, iterations: number): number {
+  const [polymer, rawRules] = input.split("\n\n");
+  const rules = readRules(rawRules);
+
+  let pairCount = gePairCount(polymer);
+  let letterCount = getLetterCount(polymer);
+
+  for (let i = 0; i < iterations; i++) {
+    [pairCount, letterCount] = step(pairCount, letterCount, rules);
+  }
+
+  const values = Array.from(letterCount.values()).sort((a, b) => a - b);
+  return values[values.length - 1] - values[0];
+}
--- day14/day14b_test.ts
+++ day14/day14b_test.ts
@@ -1,6 +1,12 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
+import { solve } from "./day14b.ts";
 
 Deno.test("Example 1", async () => {
   const input = await Deno.readTextFile("./day14/day14_ex.txt");
-  assertEquals(true, true);
+  assertEquals(solve(input), 2188189693529);
+});
+
+Deno.test("Day 14", async () => {
+  const input = await Deno.readTextFile("./day14/day14.txt");
+  assertEquals(solve(input), 3459174981021);
 });
