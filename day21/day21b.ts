9fa88896e250eacba3d0be57424b1025484502ea
Yordi Verkroost <yordi.verkroost@bettyblocks.com>
2021-12-21 14:50:21 +0100

    Day 21 part 2 in progress

--- day21/day21b.ts
+++ day21/day21b.ts
@@ -1,3 +1,78 @@
-const input = await Deno.readTextFile("./day21/day21.txt");
+const input = await Deno.readTextFile("./day21/day21_ex.txt");
 
-console.log(input);
+const memoized = new Map<string, number[]>();
+
+const delta = [
+  [1, 0, 1, 0],
+  [1, 0, 2, 0],
+  [1, 0, 3, 0],
+  [2, 0, 1, 0],
+  [2, 0, 2, 0],
+  [2, 0, 3, 0],
+  [3, 0, 1, 0],
+  [3, 0, 2, 0],
+  [3, 0, 3, 0],
+];
+
+function play(game: number[]): number[] {
+  if (game[1] >= 21) {
+    return [1, 0];
+  }
+
+  if (game[3] >= 21) {
+    return [0, 1];
+  }
+
+  const key = game.join(",");
+
+  if (memoized.has(key)) {
+    return memoized.get(key)!;
+  }
+
+  const scores = [0, 0];
+
+  for (const d of delta) {
+    const newGame = [
+      game[0] + d[0],
+      game[1] + d[1],
+      game[2] + d[2],
+      game[3] + d[3],
+    ];
+    newGame[0] = newGame[0] % 10 === 0 ? 10 : newGame[0] % 10;
+    newGame[1] += newGame[0];
+
+    newGame[2] = newGame[2] % 10 === 0 ? 10 : newGame[2] % 10;
+    newGame[3] += newGame[2];
+
+    const winner = play(newGame);
+
+    scores[0] += winner[0];
+    scores[1] += winner[1];
+  }
+
+  memoized.set(key, scores);
+
+  return scores;
+}
+
+function readGame(input: string): number[] {
+  return input.split("\n").reduce((acc: number[], line) => {
+    const [_, _id, position] = line.match(
+      /^Player (\d+) starting position: (\d+)$/,
+    )!;
+    acc.push(+position);
+    acc.push(0);
+    return acc;
+  }, []);
+}
+
+export function solve(_input: string): number {
+  const game = readGame(input);
+
+  const result = play(game);
+  console.log(result)
+
+  return 42;
+}
+
+console.log(solve(input));
--- day21/day21b_test.ts
+++ day21/day21b_test.ts
@@ -1,6 +1,12 @@
 // import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
+// import { solve } from "./day21a.ts";
 
 // Deno.test("Example 1", async () => {
 //   const input = await Deno.readTextFile("./day21/day21_ex.txt");
-//   assertEquals(true, true);
+//   assertEquals(solve(input), 739785);
+// });
+
+// Deno.test("Day 21", async () => {
+//   const input = await Deno.readTextFile("./day21/day21.txt");
+//   assertEquals(solve(input), 432450);
 // });
